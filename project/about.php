ceb5a9dee99d34078b56cdb634e3857f7d0f1f5f
Anoop21-oss-a <77046945+Anoop21-oss-a@users.noreply.github.com>
2022-07-14 09:41:49 +0530

    Add files via upload

---
+++ project/about.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>welcome</h1>
+	<p>hello </p>
+
+</body>
+</html>
---
+++ project/index.php
@@ -0,0 +1,55 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	 <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+</head>
+<body>
+	<!---<?php include 'about.php';?>--->
+	<!--<p><a href="about.php">HTML tutorial</a></p>-->
+	<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			
+  <a class="navbar-brand" href="#">HEALTHIFY</a>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="nav navbar-nav  navbar-left">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <from class="form-inline nav navbar-nav navbar-right">
+      <li class="nav-item active">
+        <a class="nav-link" href="login.php">login <span class="sr-only">(current)</span></a>
+      </li>
+  </from>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">about</a>
+      </li> 
+      <li class="nav-item">
+        <a class="nav-link disabled" href="user info.php">yoga</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#"
+        >Quote
+    </a>
+      </li>
+    </ul>
+    <form class="form-inline nav navbar-nav navbar-right">
+      <input class="form-control ml-auto" type="search" placeholder="Search" aria-label="Search">
+      <button class="nav navbar-nav navbar-right" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<style>
+body {
+  background-image: url('download.jpg');
+}
+</style>
+<p>"i support mentalhealth awareness month"</p>
+</body>
+</html>
